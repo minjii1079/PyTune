8d11feb116eba2db3d9a461fe520e3108fb8af05
kevin <kkar88314@gmail.com>
2025-09-26 22:30:42 -0400

    Update the audio recording parameters

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import pyaudio
+import os
+import numpy as np
+import scipy.fftpack
 import wave
 import keyboard
 
 # Audio params
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 1024
-WAVE_OUTPUT_FILENAME = "continuous_recording.wav"
+#FORMAT = pyaudio.paInt16
+#CHANNELS = 1
+#RATE = 44100
+#CHUNK = 1024
+#WAVE_OUTPUT_FILENAME = "continuous_recording.wav"
+
+SAMPLE_FREQUENCY = 44100
+WINDOW_SIZE = 44100 # 1 second window
+WINDOW_STEP = 21050 # 0.5 second step
+WINDOW_T_LENGTH = WINDOW_SIZE / SAMPLE_FREQUENCY # Period of the window in seconds
+SAMPLE_T_LENGTH = 1 / SAMPLE_FREQUENCY # Period of the sample in seconds
+window_samples = [0 for _ in range(WINDOW_SIZE)]
+
+
 
 audio = pyaudio.PyAudio()
 
 # Open the stream
-stream = audio.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
+stream = audio.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=SAMPLE_FREQUENCY,
                     input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=WINDOW_STEP)
 
 print("Recording started. Press 'q' to stop.")
 
@@ -24,7 +36,7 @@ frames = []
 
 # Record until 'q' is pressed (allows for continuous recording)
 while True:
-        data = stream.read(CHUNK)
+        data = stream.read(WINDOW_STEP)
         frames.append(data)
         if keyboard.is_pressed('q'): # Stop when 'q' is pressed
             print("Stopping recording.")
@@ -35,12 +47,3 @@ stream.stop_stream()
 stream.close()
 audio.terminate()
 
-# Save the recorded data as a WAV file
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(audio.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
-
-print(f"Recording saved to {WAVE_OUTPUT_FILENAME}")
